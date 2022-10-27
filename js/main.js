7b97214444c52c9450aebe36c5c030e75ea741f2
Mattmaxx04 <95719090+Mattmaxx04@users.noreply.github.com>
2022-10-27 18:49:41 -0300

    update js

--- js/main.js
+++ js/main.js
@@ -27,24 +27,7 @@ createApp({
         }
     },
     mounted() {
-        /*
-        this.modals = document.querySelectorAll("[data-modal]");
-        this.modals.forEach(function (trigger) {
-            trigger.addEventListener("click", function (event) {
-              event.preventDefault();
-              const modal = document.getElementById(trigger.dataset.modal);
-              modal.classList.add("open");
-              const exits = modal.querySelectorAll(".modal-exit");
-              exits.forEach(function (exit) {
-                exit.addEventListener("click", function (event) {
-                  event.preventDefault();
-                  modal.classList.remove("open");
-                });
-              });
-            });
-          });
-*/
-    },
+            },
     methods: {
 
         traerDatos() {
